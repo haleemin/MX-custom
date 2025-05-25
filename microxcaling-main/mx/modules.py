18f6041e9b4ef6a18cf381db183c562af9126329
haleemin <haleeho2@naver.com>
2025-05-26 00:04:00 +0900

    Update modules.py

--- microxcaling-main/mx/modules.py
+++ microxcaling-main/mx/modules.py
@@ -1,6 +1,6 @@
 # microxcaling-main/mx/modules.py
 import torch.nn as nn
-from .mx_ops import quantize_mx_op as quantize_tensor  # ← 변경된 부분
+from .mx_ops import quantize_mx_op # ← 변경된 부분
 
 class Conv2d(nn.Conv2d):
     def __init__(self, *args, mx_specs=None, **kwargs):
@@ -8,6 +8,6 @@ class Conv2d(nn.Conv2d):
         self.mx_specs = mx_specs
 
     def forward(self, x):
-        x = quantize_tensor(x, self.mx_specs, mode="input")
+        x = quantize_mx_op(x, self.mx_specs)
         out = super().forward(x)
-        return quantize_tensor(out, self.mx_specs, mode="output")
+        return quantize_mx_op(out, self.mx_specs)
